69a5db66efad3901d85966ab992b8a989983e92b
Nicolas Hasler <nicolashasler@bluewin.ch>
2023-01-12 21:31:30 +0100

    problem axios und internal server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// SongList.js
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const SongList = () => {
+  const [NFTid, setNFTid] = useState(0);
+  const [NFTartist, setNFTartist] = useState("");
+  const [NFTsongname, setNFTsongname] = useState("");
+  const [NFTduration, setNFTduration] = useState("");
+  const [NFTsongrelease, setNFTsongrelease] = useState(0);
+  const [NFTtimestamp, setNFTtimestamp] = useState("");
+  const [songs, setSongs] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(null);
+  const [borrowed, setborrowed] = useState([]);
+
+  const fetchSongs = () => {
+    console.log("fetchSongs");
+    axios.get("http://localhost:3001/songs").then((response) => {
+      console.log(response);
+      setSongs(response.data);
+    });
+  };
+
+  const handleSelect = (song) => {
+    setSelectedSong(song);
+  };
+
+  const handleBorrow = () => {
+    axios
+      .post("http://localhost:3001/borrow", {
+        NFTid: selectedSong.NFTid,
+        NFTartist: selectedSong.NFTartist,
+        NFTsongname: selectedSong.NFTsongname,
+        NFTduration: selectedSong.NFTduration,
+        NFTsongrelease: selectedSong.NFTsongrelease,
+      })
+      .then((response) => {
+        setSongs(response.data);
+      });
+    fetchSongs();
+    setSelectedSong(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchSongs}>Fetch Songs</button>
+      <ul>
+        {songs.map((song) => (
+          <li key={song.NFTid}>
+            <p>
+              {song.NFTartist +
+                " " +
+                song.NFTsongname +
+                " " +
+                song.NFTduration +
+                "min " +
+                song.NFTsongrelease}
+            </p>
+            <button onClick={() => handleSelect(song)}>Select</button>
+            {song.borrowed ? <p>Borrowed</p> : null}
+          </li>
+        ))}
+      </ul>
+      {selectedSong ? (
+        <div>
+          <p>
+            You have selected:{" "}
+            {selectedSong.NFTartist +
+              selectedSong.NFTsongname +
+              selectedSong.NFTduration +
+              selectedSong.NFTsongrelease}
+          </p>
+          <button onClick={handleBorrow}>Borrow</button>
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
-export default App;
+export default SongList;
